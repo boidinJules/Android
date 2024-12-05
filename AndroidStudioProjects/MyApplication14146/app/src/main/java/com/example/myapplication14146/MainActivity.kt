01bd7bfcdbf670230739842298dde1b72a9a1ff5
boidinJules <jules.boidin@lycee-jeanrostand.fr>
2024-12-05 12:06:16 +0100

    Exercice1

--- AndroidStudioProjects/MyApplication14146/app/src/main/java/com/example/myapplication14146/MainActivity.kt
+++ AndroidStudioProjects/MyApplication14146/app/src/main/java/com/example/myapplication14146/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.myapplication14146
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.Toast
-import android.view.Gravity
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication14146.R
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var btnRouge: Button
     private lateinit var btnVert: Button
     private lateinit var btnBleu: Button
@@ -18,35 +18,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnRouge = findViewById(R.id.buttonrouge);
+        btnBleu = findViewById(R.id.buttonviolet);
+        btnVert = findViewById(R.id.buttonvert);
+        myLayout = findViewById(R.id.FrameLayout);
 
-        // Initialisation des vues
-        btnRouge = findViewById(R.id.buttonRouge)
-        btnVert = findViewById(R.id.buttonVert)
-        btnBleu = findViewById(R.id.buttonBleu)
-        myLayout = findViewById(R.id.panelCouleur)
-
-        // OnClickListener pour le bouton Bleu
         btnBleu.setOnClickListener {
             myLayout.setBackgroundColor(Color.BLUE)
-            val myToast = Toast.makeText(this, "Il fait Bleu", Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0)
-            myToast.show()
+            showToast("Il fait Bleu")
         }
 
-        // OnClickListener pour le bouton Vert
+
         btnVert.setOnClickListener {
             myLayout.setBackgroundColor(Color.GREEN)
-            val myToast = Toast.makeText(this, "Il fait Vert", Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0)
-            myToast.show()
+            showToast("Il fait Vert")
         }
 
-        // OnClickListener pour le bouton Rouge
+
         btnRouge.setOnClickListener {
             myLayout.setBackgroundColor(Color.RED)
-            val myToast = Toast.makeText(this, "Il fait Rouge", Toast.LENGTH_SHORT)
-            myToast.setGravity(Gravity.CENTER, 0, 0)
-            myToast.show()
+            showToast("Il fait Rouge")
         }
+    }
+
+    private fun showToast(message: String) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 }
